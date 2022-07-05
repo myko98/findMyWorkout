8acbe95d597b6fad63b97214b7ab5e7420c7703c
myko <michaelluchen1998@gmail.com>
2022-07-04 20:46:06 -0400

    finished mvp

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Box, Stack, Typography, Button } from '@mui/material';
+import HeroBannerImage from '../assets/images/baki.jpg'
+
+const HeroBanner = () => {
+	return (
+		<Box sx={{
+			mt: { xs: '70px' },
+			ml: { sm: '50px' }
+		}} position="relative" p="20px">
+
+			<Typography color="#FF2625" fontWeight="600" fontSize="80px" >
+				Exercise DB
+			</Typography>
+			<Typography fontWeight="700"
+				sx={{ fontSize: { lg: '44px', xs: '40px' } }}
+				mb={6}
+				mt={5}>
+				Do the hard stuff <br /> Even if you don't feel like it.
+			</Typography>
+			<Typography fontWeight="700"
+				sx={{ fontSize: { lg: '44px', xs: '40px' } }}
+				mb={6}
+				mt={5}>
+				Explore 1000+ different <br /> exercises for various <br /> body parts
+			</Typography>
+
+			<Button variant="contained" href="#exercises"
+			>Explore Exercises</Button>
+
+
+			{/* <Typography
+				fontWeight={600}
+				color="#ff2625"
+				sx={{
+					opacity: 0.1,
+					display: { lg: 'block', xs: 'none' }
+				}}
+				fontSize="200px">
+				Gainz
+			</Typography> */}
+			<img src={HeroBannerImage} alt="" className='hero-banner-img' style={{marginTop:"-200px"}}/>
+
+		</Box>
+	)
+}
+
+export default HeroBanner
